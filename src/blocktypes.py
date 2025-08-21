7c294860b915c7991aeb0bdbb308e23c2e1bf436
argwilson <adam.rg.wilson@gmail.com>
2025-08-21 15:11:32 +0100

    updated blocktypes.py

--- src/blocktypes.py
+++ src/blocktypes.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 
 class BlockType(Enum):
@@ -18,7 +19,28 @@ def markdown_to_blocks(markdown):
         new_blocks.append(block)
     return new_blocks
 
-def block_to_block_type(markdown):
-    blocks = markdown_to_blocks(markdown)
-    for block in blocks:
-        
+def block_to_block_type(block):
+    if re.findall(r"^#{1,6} .+", block):
+        return BlockType.HEADING
+    if re.findall(r"^`{3}.+`{3}$", block):
+        return BlockType.CODE
+    verify_blocks = []
+    if re.findall(r"^>.+", block):
+        small_block = block.split("\n")
+        for small in small_block:
+            verify_blocks.append(re.findall(r"^>.+", small) != [])
+        if sum(verify_blocks) == len(small_block):
+            return BlockType.QUOTE
+    if re.findall(r"^- .+", block):
+        small_block = block.split("\n")
+        for small in small_block:
+            verify_blocks.append(re.findall(r"^- .+", small) != [])
+        if sum(verify_blocks) == len(small_block):
+            return BlockType.UNORDERED_LIST
+    if re.findall(r"1\. .+", block):
+        small_block = block.split("\n")
+        for i in range(len(small_block)):
+            verify_blocks.append(re.findall(rf"{i+1}\. .+", small_block[i]) != [])
+        if sum(verify_blocks) == len(small_block):
+            return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_blocktypes.py
+++ src/test_blocktypes.py
@@ -1,5 +1,5 @@
 import unittest
-from blocktypes import markdown_to_blocks
+from blocktypes import BlockType,markdown_to_blocks, block_to_block_type
 
 class TestSplitDelimiter(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -40,6 +40,36 @@ This is a paragraph of text. It has some **bold** and _italic_ words inside of i
                 '- This is the first list item in a list block\n- This is a list item\n- This is another list item'
             ]
         )
+
+    def test_block_to_blocktype_heading(self):
+        block = '### This is a heading'
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_blocktype_code(self):
+        block = '```This is a code block```'
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_blocktype_quote(self):
+        block = '>This is a quote\n>This is another quote\n>This is a third quote'
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_blocktype_unordered(self):
+        block = '- This is the first list item in a list block\n- This is a list item\n- This is another list item'
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_blocktype_ordered(self):
+        block = '1. This is the first list item in a list block\n2. This is a list item\n3. This is another list item'
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+    
+    #def test_block_to_blocktype_paragraph(self):
+        #block = 'This is a paragraph'
+        #block_type = block_to_block_type(block)
+        #self.assertEqual(block_type, BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
